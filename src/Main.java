42d78f357e7c1a100ef438ae167b59be8a065543
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-11-02 08:21:08 +0000

    Ultima modificación

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,33 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ProcessBuilder processBuilder= new ProcessBuilder();
-        processBuilder.command("open","-a", "Google Chrome", "http://www.google.es");
-        Process process;
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Introduce la cantidad de navegadores que quieres ejecutar: ");
+        int cantidad = lectura.nextInt();
+        ArrayList<Process> processList = new ArrayList<Process>();
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
 
         try {
-            process = processBuilder.start();
-        } catch (IOException e) {
+            for (int i = 0; i < cantidad; i++) {
+                Process process = processBuilder.start();
+                processList.add(process);
+            }
+
+            for (Process process : processList) {
+                while (process.isAlive()) {
+                    process.waitFor();
+                }
+            }
+
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 }
